8f912592e4a52c7e840e525be60f81a496bd3a4a
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-08-05 00:54:36 +0200

    17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID' : '232632633142-ko1b36s2kec1bol895u4sl6ghnfps6bu.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'CqtjpqD9fhWSTVRfc8I6x9mh',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
